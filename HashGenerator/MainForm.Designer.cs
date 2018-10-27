bcff94d6bba8183420cf9f65a0e11262d430f96d
anatoliy.karpekin <anatoliy.karpekin@R825KARPEKIN>
2018-10-27 13:19:52 +0300

    Утилита для расчета hash

---
+++ HashGenerator/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HashGenerator {
+    partial class MainForm {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SourceBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.HashButton = new System.Windows.Forms.Button();
+            this.ClipboardButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SourceBox
+            // 
+            this.SourceBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SourceBox.Location = new System.Drawing.Point(12, 29);
+            this.SourceBox.MaxLength = 40;
+            this.SourceBox.Name = "SourceBox";
+            this.SourceBox.Size = new System.Drawing.Size(410, 23);
+            this.SourceBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hash";
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultBox.Location = new System.Drawing.Point(12, 91);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(410, 56);
+            this.ResultBox.TabIndex = 1;
+            // 
+            // HashButton
+            // 
+            this.HashButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.HashButton.Location = new System.Drawing.Point(285, 157);
+            this.HashButton.Name = "HashButton";
+            this.HashButton.Size = new System.Drawing.Size(137, 28);
+            this.HashButton.TabIndex = 2;
+            this.HashButton.Text = "Расчитать";
+            this.HashButton.UseVisualStyleBackColor = true;
+            this.HashButton.Click += new System.EventHandler(this.HashButton_Click);
+            // 
+            // ClipboardButton
+            // 
+            this.ClipboardButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ClipboardButton.Location = new System.Drawing.Point(12, 157);
+            this.ClipboardButton.Name = "ClipboardButton";
+            this.ClipboardButton.Size = new System.Drawing.Size(217, 28);
+            this.ClipboardButton.TabIndex = 3;
+            this.ClipboardButton.Text = "Копировать в буфер обмена";
+            this.ClipboardButton.UseVisualStyleBackColor = true;
+            this.ClipboardButton.Click += new System.EventHandler(this.ClipboardButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 197);
+            this.Controls.Add(this.ClipboardButton);
+            this.Controls.Add(this.HashButton);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SourceBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hash generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SourceBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ResultBox;
+        private System.Windows.Forms.Button HashButton;
+        private System.Windows.Forms.Button ClipboardButton;
+    }
+}
+
---
+++ HashGenerator/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using Feodosiya.Lib.Security;
+using System;
+using System.Windows.Forms;
+
+
+namespace HashGenerator {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void HashButton_Click(object sender, EventArgs e) {
+            string src = SourceBox.Text;
+            if (string.IsNullOrWhiteSpace(src)) {
+                System.Media.SystemSounds.Beep.Play();
+                return;
+            }
+
+            try {
+                ResultBox.Text = Pbkdf2Cryptography.HashPassword(src);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClipboardButton_Click(object sender, EventArgs e) {
+            string res = ResultBox.Text;
+            if (string.IsNullOrWhiteSpace(res)) {
+                System.Media.SystemSounds.Beep.Play();
+                return;
+            }
+
+            try {
+                Clipboard.SetText(res, TextDataFormat.Text);
+                MessageBox.Show("Скопировано в буфер обмена", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
